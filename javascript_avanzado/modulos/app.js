796796bbc79cb040a27219cb7eaa41d7e197c3b3
Nayelli Gutierrez <nayelli.gutierrez.perez@gmail.com>
2022-01-24 20:23:48 -0600

    Módulos

---
+++ javascript_avanzado/modulos/app.js
@@ -0,0 +1,14 @@
+import{ sumar, multiplicar, mostrarResultado } from './calculadora.js';
+/* ./ misma carpeta
+../ una carpeta antes
+*/
+
+let resultado = sumar(10, 20)
+console.log(resultado);
+
+let resultadoMultiplicar = multiplicar(12, 46);
+
+mostrarResultado('multiplicación', 12, 46, resultadoMultiplicar);
+
+
+
