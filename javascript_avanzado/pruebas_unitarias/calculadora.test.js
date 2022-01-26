0f88c434e6e8166ec00a2df6152303ad671caf92
Nayelli Gutierrez <nayelli.gutierrez.perez@gmail.com>
2022-01-25 21:35:01 -0600

    Pruebas unitarias

--- javascript_avanzado/pruebas_unitarias/calculadora.test.js
+++ javascript_avanzado/pruebas_unitarias/calculadora.test.js
@@ -1,4 +1,4 @@
-const {sumar, restar} = require('./calculadora'); //importación
+const {sumar, restar, multiplicar, dividir} = require('./calculadora'); //importación
 
 describe('Pruebas en el componente calculadora', () => {
 
@@ -11,11 +11,15 @@ describe('Pruebas en el componente calculadora', () => {
     test('Prueba en la función restar', () =>{
 
         expect(restar(5,3)).toBe(2);
-        //espero que cuando se realice la suma de 3 y 5 me de un 8
     });
 
+    test('Prueba en la función multiplicar', () =>{
 
+        expect(multiplicar(5,3)).toBe(15);
+    });
+
+    test('Prueba en la función dividir', () =>{
+
+        expect(dividir(8,4)).toBe(2);
+    });
 });
-
-
-
